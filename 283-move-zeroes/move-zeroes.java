85b8f6a601d99cb032a86e33f36ff6f4ea0600d9
Siri Chandana <chandanasiri004@gmail.com>
2026-05-28 20:39:19 +0530

    Time: 2 ms (91.68%) | Memory: 48 MB (19.12%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,12 +1,22 @@
 class Solution {
+    public static void swappy(int nums[],int a, int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
     public void moveZeroes(int[] nums) {
-        int i=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                int t=nums[i];
-                nums[i]=nums[j];
-                nums[j]=t;
-                i++;
+        int j=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return;
+        for(int i=j+1;i<nums.length;i++){
+            if(nums[i]!=0){
+                swappy(nums,i,j);
+                j++;
             }
         }
     }
