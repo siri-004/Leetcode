7576e4cb3525e4786c2df34f978b097f818c1284
Siri Chandana <chandanasiri004@gmail.com>
2026-07-25 10:32:47 +0530

    Time: 12 ms (45.25%) | Memory: 46.1 MB (65.07%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -7,13 +7,11 @@ class Solution {
         for(int right=0;right<s.length();right++){
             freq[s.charAt(right)-'A']++;
             maxfreq=Math.max(maxfreq,freq[s.charAt(right)-'A']);
-            if((right-left+1)-maxfreq<=k){
-                ans=Math.max(ans,right-left+1);
-            }
-            else{
+            if((right-left+1)-maxfreq>k){
                 freq[s.charAt(left)-'A']--;
                 left++;
             }
+            ans=Math.max(ans,right-left+1);
         }
         return ans;
     }
