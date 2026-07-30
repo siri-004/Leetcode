693d8c6c351fdbe4f5348a08e2e7e9a78b3f07b6
Siri Chandana <chandanasiri004@gmail.com>
2026-07-30 23:00:58 +0530

    Time: 4 ms (96.92%) | Memory: 47.7 MB (66.96%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -2,43 +2,20 @@ class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int n=arr.length;
         int low=0;
-        int high=n-1;
-        while(low<=high){
+        int high=n-k;
+        while(low<high){
             int mid=low+(high-low)/2;
-            if(arr[mid]<x){
+            if(x-arr[mid]>arr[mid+k]-x){
                 low=mid+1;
             }
             else{
-                high=mid-1;
+                high=mid;
             }
         }
-        int left=high;
-        int right=low;
-        List<Integer> ans= new ArrayList<>();
-        while(k>0){
-            if(left<0){
-                ans.add(arr[right]);
-                right++;
-            }
-            else if(right>=n){
-                ans.add(arr[left]);
-                left--;
-            }
-            else{
-                int leftdist=Math.abs(arr[left]-x);
-                int rightdist=Math.abs(arr[right]-x);
-                if(leftdist<=rightdist){
-                    ans.add(arr[left]);
-                    left--;
-                }
-                else{
-                    ans.add(arr[right]);
-                    right++;
-                }
-            }
-            k--;
+        List<Integer>ans=new ArrayList<>();
+        for(int i=low;i<low+k;i++){
+            ans.add(arr[i]);
         }
-        Collections.sort(ans);
         return ans;
     }
 }
