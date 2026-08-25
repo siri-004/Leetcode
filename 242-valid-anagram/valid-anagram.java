9a00c74e3474e7ebf59b95ba19f640ab38a02a52
Siri Chandana <chandanasiri004@gmail.com>
2026-08-25 22:29:00 +0530

    Time: 6 ms (43.55%) | Memory: 44.1 MB (95.18%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,10 +3,16 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char[] a=s.toCharArray();
-        char[] b=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int count:freq){
+            if(count!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
